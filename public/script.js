160550d96758bca2e8e9043b5293feaeebbdcc12
Abraham Laria <abraham@abrahamlaria.com>
2017-07-20 22:50:17 -0700

    Added first vue object and added some functionality to the project

--- public/script.js
+++ public/script.js
@@ -1,1 +1,19 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [
+            {title: 'Item1'},
+            {title: 'Item2'},
+            {title: 'Item3'}
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function(index) {
+            this.total += 9.99;
+            this.cart.push(this.items[index]);
+            
+        }
+    }
+});

133223f748166d91e9897face2a1294fa1963b8d
Abraham Laria <abraham@abrahamlaria.com>
2017-07-20 23:33:06 -0700

    Added more functionality to the shopping cart

--- public/script.js
+++ public/script.js
@@ -1,19 +1,34 @@
+const PRICE = 9.99;
 new Vue({
     el: '#app',
     data: {
         total: 0,
         items: [
-            {title: 'Item1'},
-            {title: 'Item2'},
-            {title: 'Item3'}
+            {id: 1, title: 'Item1'},
+            {id: 2, title: 'Item2'},
+            {id: 3, title: 'Item3'}
         ],
         cart: []
     },
     methods: {
-        addItem: function(index) {
+        addItem: function (index) {
             this.total += 9.99;
-            this.cart.push(this.items[index]);
-            
+            let item = this.items[index];
+            let found = false;
+            for (let index of this.cart) {
+                if(index.id === item.id) {
+                    found = true;
+                    index.qty++;
+                }
+            }
+            if(!found) {
+                this.cart.push({
+                id: item.id,
+                title: item.title,
+                qty: 1,
+                price: PRICE
+            });
+            }           
         }
     }
 });
